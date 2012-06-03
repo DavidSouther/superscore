c72ebcf4a22d66c35de54d678716553b45f4639e
David Souther <davidsouther@gmail.com>
2012-06-03 18:19:54 -0400

    Smokescreen tests.

---
+++ test/testSuperscore.js
@@ -0,0 +1,18 @@
+/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+/*global jQuery:false, JEFRi:false, isLocal:false*/
+
+(function($){
+
+test("Unit Testing Environment", function () {
+	expect(1);
+	ok( !isLocal, "Unit tests shouldn't be run from file://, especially in Chrome. If you must test from file:// with Chrome, run it with the --allow-file-access-from-files flag!" );
+});
+
+test("Underscore utils", function(){
+	ok(_.on && _.once && _.off && _.trigger, "Underscore has additional pubsub?");
+	ok(_.ajax && _.get && _.post, "Does Underscore have additional ajax?");
+});
+
+}(jQuery));
