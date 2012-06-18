8af038f489f20279746417b4c37e5b1c29bff61e
David Souther <davidsouther@gmail.com>
2012-06-18 11:28:45 -0400

    Use Callbacks() for actual handlers.

--- src/pubsub.js
+++ src/pubsub.js
@@ -7,10 +7,14 @@
 (function(_, $){
 "use strict";
 
-// #### Pubsub with jQuery-backed eventing.
+// ### Pubsub with jQuery-backed eventing.
+// This eventing library uses pubsub channels attached to specific object instances.
+// If a channel is used with an object, it becomes associated implicitly with that object.
+// If a channel is used without an object, it is in the global pubsub scope.
 _.mixin({
-	// #### PubSub
 	// Register a function to get called when a certain event is published.
+	// Any event handlers attached after an event has been triggered at least once will
+	// immediately be called with the most recently triggered value.
 	on: function(obj, event, callback) {
 		// Use jQuery to handle DOM events.
 		if(_.isElement(obj) && $){return $(obj).on(event, callback); }
@@ -18,9 +22,14 @@ _.mixin({
 		// Use internal handler for pubsub
 		if(this.isString(obj)) {callback = event; event = obj; obj = this; }
 
-		if(this.isUndefined(obj.__event_handlers)){ obj.__event_handlers = {}; }
-		if (!(event in obj.__event_handlers)){ obj.__event_handlers[event] = []; }
-		obj.__event_handlers[event].push(callback);
+		// Ensure a container is available for all events.
+		if(!this.isObject(obj.__event_handlers)){ obj.__event_handlers = {}; }
+		// Ensure a handler is available for this particular event.
+		if (!(event in obj.__event_handlers)) {
+			// Using a memory callback
+			obj.__event_handlers[event] = _.Callbacks("memory");
+		}
+		obj.__event_handlers[event].add(callback);
 		return this;
 	},
 	// Register a function that will be called a single time when the event is published.
@@ -28,13 +37,15 @@ _.mixin({
 		// Use jQuery to handle DOM events.
 		if(_.isElement(obj) && $){return $(obj).one(event, callback); }
 
-		var removeEvent = function() { _.removeEvent(obj, event); };
+		// Turn the callback into a callback that will remove itself after getting execute.
+		var removeEvent = function() { _.off(obj, event, callback); };
 		callback = _.compose(removeEvent, callback);
 
-		// Register normally
+		// Register the self-removing callback normally.
 		this.on(obj, event, callback);
 	},
-	// Publish an event, passing args to each function registered.
+	// Publish an event, passing args to each function registered. Each callback will
+	// be executed with `obj` as their `this` context.
 	trigger: function(obj, event, args) {
 		// Use jQuery to handle DOM events.
 		if(_.isElement(obj) && $){return $(obj).trigger(event, args); }
@@ -42,12 +53,9 @@ _.mixin({
 		// Use internal handler for pubsub
 		if(this.isString(obj)) {args = event; event = obj; obj = this; }
 
-		if(this.isUndefined(obj._events)){ return; }
-		if (event in obj.__event_handlers) {
-			var events = obj.__event_handlers[event].concat();
-			for (var i = 0, ii = events.length; i < ii; i++) {
-				events[i].apply(obj, args === undefined ? [] : args);
-			}
+		// If there aren't any handlers for this event, don't do anything.
+		if(this.isObject(obj.__event_handlers) && event in obj.__event_handlers) {
+			obj.__event_handlers.fireWith(obj, args);
 		}
 		return this;
 	},
@@ -59,10 +67,10 @@ _.mixin({
 		// Use internal handler for pubsub
 		if(this.isString(obj)) { event = obj; obj = this; }
 
-		if(this.isUndefined(obj.__event_handlers)){ return; }
-		obj.__event_handlers = _.filter(obj.__event_handlers, function(cb){
-			return (cb.toString() === callback.toString());// TODO Make this smarter
-		});
+		// If there aren't any handlers for this event, don't do anything.
+		if(this.isObject(obj.__event_handlers) && event in obj.__event_handlers) {
+			obj.__event_handlers[event].remove(callback);
+		}
 		return this;
 	}
 });
